abe5f529284d04e59016dda4aacccb48ed341ee9
daehyeong2 <baconbacon1231@gmail.com>
2024-03-08 12:33:49 +0900

    WebSocket 서버 생성 (1.0 ~ 1.2)

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 import morgan from "morgan";
 import livereloadMiddleware from "connect-livereload";
@@ -18,12 +20,16 @@ app.set("views", __dirname + "/views");
 app.use(livereloadMiddleware());
 app.use(morgan("dev"));
 
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.listen(PORT, handleListening);
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+server.listen(PORT, handleListening);
