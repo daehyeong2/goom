5326d0e0683060bf3ec3ac931bec3cc0e5363284
daehyeong2 <baconbacon1231@gmail.com>
2024-03-08 11:12:59 +0900

    Setup (0.0 ~ 0.4)

---
+++ src/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import morgan from "morgan";
+import livereloadMiddleware from "connect-livereload";
+import livereload from "livereload";
+
+const liveServer = livereload.createServer({
+  exts: ["js", "pug", "css"],
+});
+
+liveServer.watch(__dirname);
+
+const app = express();
+
+const PORT = 3000;
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use(livereloadMiddleware());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListening = () =>
+  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.listen(PORT, handleListening);
