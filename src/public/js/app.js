619928fa895b6d877463d545a20b8073731c2cad
daehyeong2 <baconbacon1231@gmail.com>
2024-03-12 21:27:10 +0900

    비디오 표시, 음소거 버튼, 영상 끄기 버튼 추가 (3.0)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,75 +1,45 @@
 const socket = io();
 
-const welcome = document.getElementById("welcome");
-const form = document.querySelector("form");
-const room = document.getElementById("room");
-const message = document.getElementById("message");
-const roomList = document.getElementById("rooms");
+const myVideo = document.getElementById("myVideo");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
 
-room.hidden = true;
+let myStream;
+let muted = false;
+let cameraOff = false;
 
-let roomName;
-
-function paintTitle(newCount) {
-  const h3 = room.querySelector("h3");
-  h3.innerText = `"${roomName}" Room (${newCount})`;
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    myVideo.srcObject = myStream;
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-function addMessage(message) {
-  const ul = room.querySelector("ul");
-  const li = document.createElement("li");
-  li.innerText = message;
-  ul.append(li);
+getMedia();
+
+function handleMuteClick() {
+  if (muted) {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  } else {
+    muteBtn.innerText = "UnMute";
+    muted = true;
+  }
+}
+function handleCameraClick() {
+  if (cameraOff) {
+    cameraBtn.innerText = "Turn Camera Off";
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = "Turn Camera On";
+    cameraOff = true;
+  }
 }
 
-function handleMessageSubmit(event) {
-  event.preventDefault();
-  const input = message.querySelector("input");
-  const value = input.value;
-  socket.emit("new_message", value, roomName, () => {
-    addMessage(`You: ${value}`);
-  });
-  input.value = "";
-}
-
-function showRoom(newCount) {
-  welcome.hidden = true;
-  room.hidden = false;
-  paintTitle(newCount);
-  message.addEventListener("submit", handleMessageSubmit);
-}
-
-function handleRoomSubmit(event) {
-  event.preventDefault();
-  const roomInput = form.querySelector("#roomInput");
-  const nameInput = form.querySelector("#nameInput");
-  if (roomInput.value === "" || nameInput.value === "") return;
-  socket.emit("nickname", nameInput.value);
-  socket.emit("enter_room", roomInput.value, showRoom);
-  roomName = roomInput.value;
-  roomInput.value = "";
-  nameInput.value = "";
-}
-
-form.addEventListener("submit", handleRoomSubmit);
-
-socket.on("join", (user, newCount) => {
-  addMessage(`${user} joined!`);
-  paintTitle(newCount);
-});
-
-socket.on("leave", (user, newCount) => {
-  addMessage(`${user} left.`);
-  paintTitle(newCount);
-});
-
-socket.on("new_message", addMessage);
-
-socket.on("room_change", (rooms) => {
-  roomList.innerHTML = "";
-  rooms.forEach((roomName) => {
-    const li = document.createElement("li");
-    li.innerText = roomName;
-    roomList.append(li);
-  });
-});
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
